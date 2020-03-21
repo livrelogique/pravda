0d40c76d57643df5e010bbb40c3a3335c2b80254
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-03-21 10:51:10 +0100

    beginning resolution FO (not working)

--- src/ProofSystem.ts
+++ src/ProofSystem.ts
@@ -1,30 +1,25 @@
+import { Formula } from './Formula';
 import { Proof, stringToProof } from "./Proof.js";
 
+type rule0 = (Formula) => (string | boolean);
+type rule1 = (f1: Formula, f2: Formula) => (string | boolean);
+type rule2 = (f1: Formula, f2: Formula, f3: Formula) => (string | boolean);
 
 export class ProofSystem {
-    private rules0: any[] = new Array();
-    private rules1: any[] = new Array();
-    private rules2: any[] = new Array();
+    private rules0: rule0[] = new Array();
+    private rules1: rule1[] = new Array();
+    private rules2: rule2[] = new Array();
 
-    protected addRule0(test) {
-        this.rules0.push(test);
-    }
-
-    protected addRule1(test) {
-        this.rules1.push(test);
-    }
-
-    protected addRule2(test) {
-        this.rules2.push(test);
-    }
+    protected addRule0(test: rule0) { this.rules0.push(test); }
+    protected addRule1(test: rule1) { this.rules1.push(test); }
+    protected addRule2(test: rule2) { this.rules2.push(test); }
 
     public checkProof(proof: Proof) {
 
         for (let rule of this.rules0)
             for (let i = 0; i < proof.length; i++) if (proof.formulas[i] && proof.justification[i] == "???") {
                 let output = rule(proof.formulas[i]);
-                if (output)
-                    proof.justification[i] = output;
+                if (output) proof.justification[i] = <string>output;
             }
 
 
@@ -32,8 +27,7 @@ export class ProofSystem {
             for (let i = 0; i < proof.length; i++) if (proof.formulas[i] && proof.justification[i] == "???")
                 for (let j = 0; j < i; j++) if (proof.formulas[j]) {
                     let output = rule(proof.formulas[j], proof.formulas[i]);
-                    if (output)
-                        proof.justification[i] = output + " (" + (j + 1) + ")";
+                    if (output) proof.justification[i] = output + " (" + (j + 1) + ")";
                 }
 
 
@@ -42,8 +36,7 @@ export class ProofSystem {
                 for (let j = 0; j < i; j++) if (proof.formulas[j])
                     for (let k = 0; k < j; k++) if (proof.formulas[k]) {
                         let output = rule(proof.formulas[k], proof.formulas[j], proof.formulas[i]);
-                        if (output)
-                            proof.justification[i] = output + " (" + (k + 1) + " , " + (j + 1) + ")";
+                        if (output) proof.justification[i] = output + " (" + (k + 1) + " , " + (j + 1) + ")";
                     }
 
     }
