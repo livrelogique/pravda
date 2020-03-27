82bfe4de1a8d42825ebcfb35cd9bfaf3c76f7f46
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-03-27 20:18:57 +0100

    natural deduction: rules for or

--- src/ProofSystem.ts
+++ src/ProofSystem.ts
@@ -8,6 +8,7 @@ export type RuleOutputTmp = { type: "success", msg: string } | { type: "issue", 
 type Rule0 = (Formula) => RuleOutput;
 type Rule1 = (f1: Formula, f2: Formula) => RuleOutput;
 type Rule2 = (f1: Formula, f2: Formula, f3: Formula) => RuleOutput;
+type Rule3 = (f1: Formula, f2: Formula, f3: Formula, f4: Formula) => RuleOutput;
 
 
 
@@ -24,10 +25,12 @@ export class ProofSystem {
     private rules0: Rule0[] = new Array();
     private rules1: Rule1[] = new Array();
     private rules2: Rule2[] = new Array();
+    private rules3: Rule3[] = new Array();
 
     protected addRule0(test: Rule0) { this.rules0.push(test); }
     protected addRule1(test: Rule1) { this.rules1.push(test); }
     protected addRule2(test: Rule2) { this.rules2.push(test); }
+    protected addRule3(test: Rule3) { this.rules3.push(test); }
 
     public checkProof(proof: Proof) {
         for (let i = 0; i < proof.length; i++) if (proof.formulas[i] && proof.justification[i].indexOf("???") >= 0) {
@@ -59,6 +62,17 @@ export class ProofSystem {
                             proof.justification[i] = output.msg + " (" + (k + 1) + " , " + (j + 1) + ")";
                         }
                     }
+
+
+            for (let rule of this.rules3)
+                for (let j = 0; j < i; j++) if (proof.formulas[j])
+                    for (let k = 0; k < j; k++) if (proof.formulas[k])
+                        for (let l = 0; l < k; l++) if (proof.formulas[l]) {
+                            let output = rule(proof.formulas[l], proof.formulas[k], proof.formulas[j], proof.formulas[i]);
+                            if (output) {
+                                proof.justification[i] = output.msg + " (" + (k + 1) + " , " + (j + 1) + ", " + (l + 1) + ")";
+                            }
+                        }
 
         }
 
