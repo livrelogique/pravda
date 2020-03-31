6858f6da776bffe0aeee82acaa683069b87cd8cd
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-03-31 22:06:35 +0200

    cleaning the code

--- src/PatternMatching.ts
+++ src/PatternMatching.ts
@@ -3,42 +3,43 @@ import * as Utils from "./Utils.js";
 import { ProofSystem, RuleOutput, RuleOutputTmp } from "./ProofSystem.js";
 import { Substitution } from "./Substitution.js"
 
+
+/**
+ * 
+ * @param formula 
+ * @param pattern 
+ * @param initialSubstitution 
+ * 
+ * @returns a substitution sub such that the formula is obtained from the pattern by applying that substitution. If this is not possible it returns null. If the substitution is null, it returns null. In clear, it does pattern matching.
+ */
 export function patternMatchingFormula(formula: Formula, pattern: Formula, initialSubstitution: Substitution = {}): Substitution {
-    function isVariablePattern(s) {
-        //   return ["phi", "psi", "chi", "eta", "Gamma", "Gamma+", "Delta", "Sigma"].indexOf(s) >= 0;
-        return typeof s == "string";
-    }
+    function isVariablePattern(s) { return typeof s == "string"; }
+
     function patternMatchingFormula2(formula: Formula, pattern: Formula, sub: Substitution) {
         if (isVariablePattern(pattern)) {
             let v = <string>(<any>pattern);
-            if (sub[v] == undefined)
-                sub[v] = formula;
+            if (sub[v] == undefined) sub[v] = formula;
             else if (!Utils.same(formula, sub[v]))
                 throw "pattern matching error because not the same formula registered in the substitution";
         }
         else {
-
             if (formula.type != pattern.type)
                 throw "pattern matching error because different type: " + formula.type + " VS " + pattern.type + "(pattern is " + pattern + ")";
 
             if ((<any[]>formula.args).length != (<any[]>pattern.args).length)
                 throw "pattern matching error because not the same number of arguments";
-            for (let i in formula.args) {
+            for (let i in formula.args)
                 patternMatchingFormula2(formula.args[i], pattern.args[i], sub);
-            }
         }
     }
 
+    //enables to call patternMatchingFormula on several patterns ;)
     if (initialSubstitution == null) return null;
 
     let sub = initialSubstitution;
 
-    try {
-        patternMatchingFormula2(formula, pattern, sub);
-    }
-    catch (e) {
-        return null;
-    }
+    try { patternMatchingFormula2(formula, pattern, sub); }
+    catch (e) { return null; }
     return sub;
 }
 
